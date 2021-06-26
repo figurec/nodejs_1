f1ad667f7404d8088cf00622861bb3975dfbef2f
figurec <86502121+figurec@users.noreply.github.com>
2021-06-26 05:30:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+v = 0;
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  str = JSON.stringify(res.getHeaders()) + "\n";
+  str += res.statusCode + "\n";
+  str += ++v;
+  res.end('Hello World\n' + str);
 });
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
